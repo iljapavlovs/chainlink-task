d92f1f0bf7c4d4cb18685b276d0cca209c874556
ilja.pavlovs <ilja.pavlov@technest.es>
2022-03-24 22:21:41 +0200

    adding kovan network to sh script

--- deployGetterSetterContract.sh
+++ deployGetterSetterContract.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Installing all needed dependencies..."
-npm ci
+npm install
 echo "Starting deployment of GetterSetter contract"
-npx hardhat run scripts/deploy.ts
+npx hardhat run scripts/deploy.ts --network kovan
 echo "Deployment process finished successfully!"
